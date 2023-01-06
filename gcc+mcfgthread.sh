9b3e553ace119336e931e26212a1c645ed92f5cb
eko5624 <562433403@qq.com>
2023-01-06 17:40:14 +0800

    Update gcc+mcfgthread.sh

--- gcc+mcfgthread.sh
+++ gcc+mcfgthread.sh
@@ -3,20 +3,23 @@ set -ex
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+rm -rf $DIR/mingw-w64-headers-git
+rm -rf $DIR/mingw-w64-crt-git
 rm -rf $DIR/mingw-w64-mcfgthread
-rm -rf $DIR/gcc-12-with-mcfgthread
+rm -rf $DIR/mingw-w64-gcc
+rm -rf $DIR/mingw-w64-gdb
 rm -rf $DIR/gcc-13-with-mcfgthread
-mkdir -p $DIR/gcc-12-with-mcfgthread
 mkdir -p $DIR/gcc-13-with-mcfgthread
 
 # get 'mingw-w64-mcfgthread' 'mingw-w64-gcc'
 git clone https://github.com/lhmouse/MINGW-packages.git
 cd $DIR/MINGW-packages
 git branch -a
-git checkout -b mcfgthread-pkg
+cp -rf mingw-w64-headers-git ../
+cp -rf mingw-w64-crt-git ../
 cp -rf mingw-w64-mcfgthread ../
-git checkout master
-cp -rf mingw-w64-gcc ../gcc-12-with-mcfgthread
+cp -rf mingw-w64-gcc ../
+cp -rf mingw-w64-gdb ../
 
 git checkout -b gcc-13 origin/gcc-13
 cp -rf mingw-w64-gcc ../gcc-13-with-mcfgthread
