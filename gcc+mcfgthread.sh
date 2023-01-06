8479f29953c5e7c253bccfdd7140448cae3796c7
eko5624 <562433403@qq.com>
2023-01-06 16:31:02 +0800

    Update gcc+mcfgthread.sh

--- gcc+mcfgthread.sh
+++ gcc+mcfgthread.sh
@@ -10,11 +10,11 @@ git clone https://github.com/lhmouse/MINGW-packages.git
 cd $DIR/MINGW-packages
 git branch -a
 git checkout -b mcfgthread-pkg
-cp -rf MINGW-packages/mingw-w64-mcfgthread ../gcc-12-with-mcfgthread
+cp -rf mingw-w64-mcfgthread ../gcc-12-with-mcfgthread
 git checkout -b master
-cp -rf MINGW-packages/mingw-w64-gcc ../gcc-12-with-mcfgthread
+cp -rf mingw-w64-gcc ../gcc-12-with-mcfgthread
 
 git checkout -b gcc-13
-cp -rf MINGW-packages/mingw-w64-gcc ../gcc-13-with-mcfgthread
+cp -rf mingw-w64-gcc ../gcc-13-with-mcfgthread
 cd ..
 rm -rf MINGW-packages
