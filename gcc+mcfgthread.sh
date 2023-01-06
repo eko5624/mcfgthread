c4189b5b9602dd78c948dfe85c026400904cd023
eko5624 <562433403@qq.com>
2023-01-06 15:58:43 +0800

    Update and rename gcc-12+mcfgthread.sh to gcc+mcfgthread.sh

--- gcc+mcfgthread.sh
+++ gcc+mcfgthread.sh
@@ -3,15 +3,18 @@ set -ex
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-rm -rf $DIR/mingw-w64*
+rm -rf $DIR/*with-mcfgthread
 
 # get 'mingw-w64-mcfgthread' 'mingw-w64-gcc'
 git clone https://github.com/lhmouse/MINGW-packages.git
 cd $DIR/MINGW-packages
 git branch -a
 git checkout -b mcfgthread-pkg
-cp -rf MINGW-packages/mingw-w64-mcfgthread ../
+cp -rf MINGW-packages/mingw-w64-mcfgthread/* ../gcc-12-with-mcfgthread
 git checkout -b master
-cp -rf MINGW-packages/mingw-w64-gcc ../
+cp -rf MINGW-packages/mingw-w64-gcc/* ../gcc-12-with-mcfgthread
+
+git checkout -b gcc-13
+cp -rf MINGW-packages/mingw-w64-gcc/* ../gcc-13-with-mcfgthread
 cd ..
 rm -rf MINGW-packages
