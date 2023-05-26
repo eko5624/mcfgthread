2ad892fec8fb065202430f860b49328d4c5942be
eko5624 <562433403@qq.com>
2023-05-26 18:03:35 +0800

    Update gcc+mcfgthread.sh

--- gcc+mcfgthread.sh
+++ gcc+mcfgthread.sh
@@ -7,8 +7,11 @@ rm -rf $DIR/mingw-w64-binutils
 rm -rf $DIR/mingw-w64-headers-git
 rm -rf $DIR/mingw-w64-crt-git
 rm -rf $DIR/mingw-w64-mcfgthread
+rm -rf $DIR/mingw-w64-winpthreads-git
+rm -rf $DIR/mingw-w64-libiconv
 rm -rf $DIR/mingw-w64-gcc
 rm -rf $DIR/mingw-w64-gdb
+
 
 git clone https://github.com/lhmouse/MINGW-packages.git
 cd $DIR/MINGW-packages
@@ -16,6 +19,8 @@ cp -rf mingw-w64-binutils ../
 cp -rf mingw-w64-headers-git ../
 cp -rf mingw-w64-crt-git ../
 cp -rf mingw-w64-mcfgthread ../
+cp -rf mingw-w64-winpthreads-git ../
+cp -rf mingw-w64-libiconv ../
 cp -rf mingw-w64-gcc ../
 cp -rf mingw-w64-gdb ../
 cd ..
